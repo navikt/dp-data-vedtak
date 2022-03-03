f81a715f7a94bcf37b3f403b1e282599b739c41b
André Roaldseth <andre.roaldseth@nav.no>
2022-03-03 21:41:17 +0100

    Fiks config

--- app/src/main/kotlin/no/nav/dagpenger/data/vedtak/Config.kt
+++ app/src/main/kotlin/no/nav/dagpenger/data/vedtak/Config.kt
@@ -6,15 +6,14 @@ import com.natpryce.konfig.EnvironmentVariables
 import com.natpryce.konfig.overriding
 
 private val defaultProperties = ConfigurationMap(
-    mapOf(
-        "HTTP_PORT" to "8080",
-        "RAPID_APP_NAME" to "dp-data-vedtak",
-        "KAFKA_RAPID_TOPIC" to "teamdagpenger.rapid.v1",
-        "KAFKA_RESET_POLICY" to "earliest",
-    )
+    "HTTP_PORT" to "8080",
+    "RAPID_APP_NAME" to "dp-data-vedtak",
+    "KAFKA_RAPID_TOPIC" to "teamdagpenger.rapid.v1",
+    "KAFKA_RESET_POLICY" to "earliest",
 )
+
 internal val config: Configuration by lazy {
     EnvironmentVariables overriding defaultProperties
 }
 
-fun Configuration.asMap(): Map<String, String> = list().reversed().fold(emptyMap()) { map, pair -> map + pair.second }
+internal fun Configuration.asMap(): Map<String, String> = list().reversed().fold(emptyMap()) { map, pair -> map + pair.second }
